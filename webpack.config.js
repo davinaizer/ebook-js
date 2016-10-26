5ab280c547c89f20ddfd942927dc18ed4c971966
Davi Naizer <davinaizer@gmail.com>
2016-10-26 11:03:42 -0200

    Webpack ScrollMagic fix

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,16 @@ module.exports.development = {
       app: 'src/app',
       content: 'src/app/content',
       core: 'src/app/core',
-      libs: 'src/app/libs'
+      libs: 'src/app/libs',
+
+      //-- SCROLLMAGIC fix
+      'TweenLite': path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+      'TweenMax': path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+      'TimelineLite': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+      'TimelineMax': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+      'ScrollMagic': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+      'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+      'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
     }
   },
   plugins: [
@@ -76,7 +85,16 @@ module.exports.production = {
       app: 'src/app',
       content: 'src/app/content',
       core: 'src/app/core',
-      libs: 'src/app/libs'
+      libs: 'src/app/libs',
+
+      //-- SCROLLMAGIC fix
+      'TweenLite': path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+      'TweenMax': path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+      'TimelineLite': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+      'TimelineMax': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+      'ScrollMagic': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+      'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+      'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
     }
   },
   plugins: [
